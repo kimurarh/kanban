b49948bd13af98dbd06e2c711188d54642f79af0
Rafael Kimura <rafaelkimura94@gmail.com>
2020-07-17 20:29:38 -0300

    updates to functionality and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+$(function() {
+
+    function addNewObjective() {
+        var objectiveInput = document.getElementById("new-objective");
+        var toDo = document.getElementById("to-do");
+
+        if (objectiveInput.value != "") {
+            toDo.innerHTML += "<div id='task'class='card' draggable='true' ondragstart='drag(event)'>" + objectiveInput.value + "<div class='close-icon'><span class='tooltip-text'>close</span></div></div>"
+        }
+        objectiveInput.value = "";
+
+        function checkObjective() {
+            $(".close-icon").click(function() {
+                $(this).parent().remove();
+            });
+        }
+
+        checkObjective();
+    }
+
+    // Add text when button is clicked
+    $("#new-objective-button").click(() => addNewObjective());
+
+    // Trigger add button when 'Enter' is pressed 
+    $("#new-objective").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#new-task-button").click();
+        }
+    })
+})
+
 // Prevents the browser from preventing the drop action
 function allowDrop(event) {
     event.preventDefault();
